309681e0cc31bd5fac9a6ac5c4e268576f7d825e
Niels Koster <n.koster@portavita.eu>
2021-09-03 01:12:36 +0200

    Material layout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
+import { TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-const Error = _ => {
-  return <p>something went wrong, it might be you</p>
-}
+const Login = ({setTokens, inactive}) => {
 
-const Login = ({setTokens}) => {
+  const Error = _ => {
+    return <p>Login failure</p>
+  }
+  
+  const Inactive = _ => {
+    inactive.current = false
+    return <p>Logged out due to inactivity</p>
+  }
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [err, setErr] = useState(false)
+  
+  const classes = useStyles()
 
   const onChangeUsername = evt => setUsername(evt.target.value)
   const onChangePassword = evt => setPassword(evt.target.value)
@@ -39,24 +48,55 @@ const Login = ({setTokens}) => {
       setErr(true)
     })
   }
+
   return (
-    <>
-      {err && <Error />}
-      <form onSubmit={onLogin} >
-      <input 
-        type='text'
-        name='username'
-        onChange={onChangeUsername}
-      />
-      <input
-        type='password'
-        name='password'
-        onChange={onChangePassword}
-      />
-      <input type='submit' value='login'/>
-      </form>
-    </>
+    <div style={{maxWidth: '600px'}}>
+      <div style={{fontSize: '16px', textAlign: 'center'}}>
+        {err && <Error />}
+        {inactive.current && !err && <Inactive />}
+      </div>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <form onSubmit={onLogin} >
+          <div className={classes.box} >
+            <TextField
+              type='text'
+              name='username'
+              label='Username'
+              className={classes.root}
+              onChange={onChangeUsername}
+            />
+            <TextField
+              type='password'
+              name='password'
+              label='Password'
+              className={classes.root}
+              onChange={onChangePassword}
+            />
+            <Button
+              color='primary'
+              variant='outlined'
+              type='submit'
+              className={classes.root}
+              disabled={username.length < 1 || password.length < 1 && true}
+              >Login</Button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
+
+const useStyles = makeStyles({
+  root: {
+    borderRadius: 3,
+    margin: '10px',
+  },
+  box: {
+    marginTop: '100px',
+    maxWidth: '700px',
+    display: 'flex',
+    alignItems: 'baseline'
+  }
+})
 
 export default Login
